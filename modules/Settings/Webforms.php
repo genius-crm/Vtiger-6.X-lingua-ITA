e3fbcaae5165b4434c34d84861d6c8e0a2879132
ardizeneli <ardizeneli@users.noreply.github.com>
2015-09-02 11:59:22 +0200

    Update Webforms.php

--- modules/Settings/Webforms.php
+++ modules/Settings/Webforms.php
@@ -16,16 +16,16 @@ $languageStrings = array(
 	'Return Url'                   => 'Url di ritorno'                  , // TODO: Review
 	'Post Url'                     => 'Post Url'                    , // TODO: Review
 	'SINGLE_Webforms'              => 'Webform'                     , // TODO: Review
-	'LBL_SHOW_FORM'                => 'MOstra form'                   , // TODO: Review
+	'LBL_SHOW_FORM'                => 'Mostra form'                   , // TODO: Review
 	'LBL_DUPLICATES_EXIST'         => 'Nome del Webform esistente' , // TODO: Review
 	'LBL_WEBFORM_INFORMATION'      => 'Informazioni Webform'         , // TODO: Review
 	'LBL_FIELD_INFORMATION'        => 'Informazioni campo'           , // TODO: Review
 	'LBL_FIELD_NAME'               => 'Nome campo'                  , // TODO: Review
 	'LBL_OVERRIDE_VALUE'           => 'Sovrascrivi valore'              , // TODO: Review
 	'LBL_MANDATORY'                => 'Obbligatorio'                   , // TODO: Review
-	'LBL_WEBFORM_REFERENCE_FIELD'  => 'Compo di riferimento Webform'    , // TODO: Review
+	'LBL_WEBFORM_REFERENCE_FIELD'  => 'Campo di riferimento Webform'    , // TODO: Review
 	'LBL_SELECT_FIELDS_OF_TARGET_MODULE' => 'Seleziona campi per modulo target...', // TODO: Review
-	'LBL_ALLOWS_YOU_TO_MANAGE_WEBFORMS' => 'Allows you to manage webforms', // TODO: Review
+	'LBL_ALLOWS_YOU_TO_MANAGE_WEBFORMS' => 'Ti permette di gestire i webforms', // TODO: Review
 	'LBL_ADD_FIELDS'               => 'Aggiungi campi'                  , // TODO: Review
 	'LBL_EMBED_THE_FOLLOWING_FORM_IN_YOUR_WEBSITE' => 'Incorpora il seguente form nel tuo sito web', // TODO: Review
 	'LBL_SELECT_VALUE'             => 'Seleziona valore'                , // TODO: Review
@@ -33,6 +33,6 @@ $languageStrings = array(
 );
 $jsLanguageStrings = array(
 	'JS_WEBFORM_DELETED_SUCCESSFULLY' => 'Webform cancellato', // TODO: Review
-	'JS_LOADING_TARGET_MODULE_FIELDS' => 'Loadding Target Module Fields', // TODO: Review
+	'JS_LOADING_TARGET_MODULE_FIELDS' => 'Caricamento dei campi del modulo obiettivo', // TODO: Review
 	'JS_SELECT_VALUE'              => 'Seleziona valore'                , // TODO: Review
 );
